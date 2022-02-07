e3ea3e41b12fc789b8e86ad9bb9703fef6f0a6e1
Roshan Paudel <rozanpoudel@gmail.com>
2022-02-07 12:02:38 +0545

    little changes

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const path = require("path");
+const Message = require("./model/message");
 
 //App initialization
 const app = express();
 
 //mongoose connection here
+const remoteURI =
+  "mongodb+srv://linux:roshan@cluster0.gvnig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
+const localURI = "mongodb://127.0.0.1:27017/contacts";
 mongoose
-  .connect("mongodb://127.0.0.1:27017/portfolio")
+  .connect(remoteURI)
   .then(() => {
     console.log("Connection Established Successfully");
   })
@@ -32,6 +36,23 @@ app.get("/", (req, res) => {
   res.render("client/index");
 });
 
+app.post("/contact", async (req, res) => {
+  const newMessage = new Message(req.body);
+  await newMessage
+    .save()
+    .then(() => {
+      console.log("Saved!!!");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  res.redirect("/");
+});
+
+app.get("/admin", async (req, res) => {
+  const messages = await Message.find({});
+  res.render("admin/auth");
+});
 //App is listening to http://localhost:3000/
 app.listen(3000, () => {
   console.log("Listening to port 3000");
