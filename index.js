f6b8f5095c619c021f38eb3afc27594dea977af9
Roshan Paudel <rozanpoudel@gmail.com>
2022-02-09 07:55:23 +0545

    admin edit

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
 //All dependencies here.....
-const express = require("express");
-const mongoose = require("mongoose");
-const methodOverride = require("method-override");
-const path = require("path");
-const Message = require("./model/message");
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const path = require('path');
+const Message = require('./model/message');
 
 //App initialization
 const app = express();
 
 //mongoose connection here
 const remoteURI =
-  "mongodb+srv://linux:roshan@cluster0.gvnig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  'mongodb+srv://linux:roshan@cluster0.gvnig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const localURI = "mongodb://127.0.0.1:27017/contacts";
+const localURI = 'mongodb://127.0.0.1:27017/contacts';
 mongoose
   .connect(remoteURI)
   .then(() => {
-    console.log("Connection Established Successfully");
+    console.log('Connection Established Successfully');
   })
   .catch((e) => {
     console.log(e);
   });
 
 //Setting up view enjine as ejs
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 //All middlewares here
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+const checkPassword = async (req, res) => {
+  const { username, password } = req.body;
+  if (username === 'roshan' && password === 'machinegun') {
+    const messages = await Message.find({});
+    res.render('admin/index', { messages });
+  } else {
+    res.send('Invalid Password');
+  }
+};
 
 //All routes here
-app.get("/", (req, res) => {
-  res.render("client/index");
+app.get('/', (req, res) => {
+  res.render('client/index');
 });
 
-app.post("/contact", async (req, res) => {
+app.post('/contact', async (req, res) => {
   const newMessage = new Message(req.body);
   await newMessage
     .save()
     .then(() => {
-      console.log("Saved!!!");
+      console.log('Saved!!!');
     })
     .catch((e) => {
       console.log(e);
     });
-  res.redirect("/");
+  res.redirect('/');
 });
 
-app.get("/admin", async (req, res) => {
-  const messages = await Message.find({});
-  res.render("admin/auth");
+app.get('/admin', async (req, res) => {
+  res.render('admin/auth');
 });
+
+app.post('/auth', (req, res) => {
+  const { username } = req.body;
+  const { password } = req.body;
+  res.redirect('admin/index');
+});
+app.get('/admin/index', checkPassword, async (req, res) => {});
 //App is listening to http://localhost:3000/
 app.listen(3000, () => {
-  console.log("Listening to port 3000");
+  console.log('Listening to port 3000');
 });
